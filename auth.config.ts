682bc6c6d8f3abe47e23318e64252fd4fa9e5405
surfiniaburger <jdmasciano2@gmail.com>
2024-01-08 15:20:11 +0100

    config

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,7 @@ import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
   pages: {
-    signIn: '/login',
+    signIn: '/dashboard',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
